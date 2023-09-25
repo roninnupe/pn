2e42aa22cfc255fb311987ffb7113c1ff3891b2d
roninnupe <roninnnupe@gmail.com>
2023-09-24 23:34:00 -0400

    gallery updates with URL

--- 9_pirate_images.py
+++ 9_pirate_images.py
@@ -4,7 +4,7 @@ import pn_helper as pn
 
 app = Flask(__name__)
 
-# Load data from the Excel file and extract the 'imageUrl' column
+# Load data from the Excel file and extract the 'imageUrl' and 'tokenId' columns
 data = pd.read_excel(pn.data_path("pn_pirates.xlsx"))
 
 # Transform the image URLs to the IPFS gateway format
@@ -14,12 +14,15 @@ def transform_url(url):
     else:
         return url  # If not 'ipfs://', leave it as is
 
-# Get the image URLs as a list
-image_urls = data['imageUrl'].apply(transform_url).tolist()
+# Apply the URL transformation to the 'imageUrl' column and create a new 'imageUrl' column
+data['imageUrl'] = data['imageUrl'].apply(transform_url)
+
+# Get the image data as a list of dictionaries
+image_data = data.to_dict(orient='records')
 
 @app.route('/')
 def index():
-    html_content = render_template('index.html', image_urls=image_urls)
+    html_content = render_template('index.html', image_data=image_data)
 
     # Save the HTML content to a file named 'pirate.html'
     with open(pn.data_path('pirate.html'), 'w', encoding='utf-8') as file:
